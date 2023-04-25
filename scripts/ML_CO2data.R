ae48ac19cb8c53d71c55ea8a7b9add30847cf62a
JET <thomleyje@appstate.edu>
2023-04-24 22:54:24 -0400

    Upload scripts with instructions

--- scripts/ML_CO2data.R
+++ scripts/ML_CO2data.R
@@ -11,15 +11,39 @@
 # --------------------------------------------------
 # read the Mauna Loa data from the web
 
+# x-----INSTRUCTIONS FOR THIS SECTION-----x
+# Delete the instructions when you are done
+# Use the code we developed in class to read
+# the Mauna Loa CO2 data from the web (from
+# "readingwritingworksheet.Rmd" in the repo
+# STT2860manipulation. Use the version that
+# specifies the column types and replaces 
+# all of the NA values with R's NA symbol.
+# x---------------------------------------x
 
+PUT YOUR CODE HERE!
 
 # --------------------------------------------------
 # Format, tidy, and reshape the dataset
 
+# x-----INSTRUCTIONS FOR THIS SECTION-----x
+# Delete the instructions when you are done
+# Create a new variable that unites data in
+# the year and month columns in the format
+# YEAR-MONTH. Pivot the data so that both
+# CO2 measurements are in the same column.
+# x---------------------------------------x
 
+PUT YOUR CODE HERE!
 
 # --------------------------------------------------
-# save the tidied dataset to data_tidy
+# save the tidied data as .rds to data_tidy
 
+# x-----INSTRUCTIONS FOR THIS SECTION-----x
+# Delete the instructions when you are done
+# Save the dataset as an .rds file into the
+# data_tidy folder.
+# x---------------------------------------x
 
+PUT YOUR CODE HERE!
 
